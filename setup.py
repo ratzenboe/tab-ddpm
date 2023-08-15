0293b98b514be8e28124d682bf86fe58909d2f8b
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-08-15 22:32:10 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
-        'License :: MIT License',  
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
